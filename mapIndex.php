0188717eab2f6766176083c026c33012a88e68e5
Chris Maness <chrismaness@Coeus.local>
2013-11-17 02:14:52 -0500

    fixed up main page

--- mapIndex.php
+++ mapIndex.php
@@ -11,10 +11,12 @@
       src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCijtc1i-7u_rcwAHIeEDKXSZP3BViE_D0&sensor=false">
     </script>
     <script type="text/javascript">
+    
+      //Just the basic set up for a google map that doesn't have any of the controls visible. 
       function initialize() {
         var mapOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
-          zoom: 8,
+          center: new google.maps.LatLng(-33.861133,151.214038),
+          zoom: 16,
           mapTypeId: google.maps.MapTypeId.ROADMAP,
           panControl:false,
           scaleControl: false,
@@ -25,28 +27,59 @@
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
       }
+      
+      //Displays the map once the window loads
       google.maps.event.addDomListener(window, 'load', initialize);
+      
     </script>
-    <link rel="stylesheet" href="css/jquery.sidr.light.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="css/jquery.sidr.dark.css">
     <style>
     	/* Hides the map and satellite view buttons. */
     	.gmnoprint{display:none}
+    	
+    	.navbar-inner{
+       		-webkit-border-radius: 0; -moz-border-radius: 0; border-radius: 0;
+    	}
+    	
+    	.navbar{
+    		margin-bottom:0px;
+    	}
+    	
+    	.navbar .brand {
+		  margin-left: auto;
+		  padding-left:45px;
+		  margin-right: auto;
+		  width: 160px;
+		  float: none;
+		  display: block;
+		  margin-top: -35px;
+		  color:white;
+		}
     </style>
   </head>
   <body>
-
+  	<div class="navbar navbar-inverse">
+  		<div class="navbar-inner">
+  		
+			<a class="btn btn-inverse" id="simple-menu" href="#sidr" type="button">
+				<i class="icon-th-list icon-white"></i>
+			</a>
+			
+			<span class="brand">ETSU CityExplore</span>
+		
+	  </div>
+	</div>
     <div style="height:100%; width:100%;">
-    	<div style="z-index:9999; position:absolute; padding-top: 20px;">
-    		<a id="simple-menu" href="#sidr"><img src="./tab.png" /></a>
-    	</div>
+    	<!-- Div for the map -->
     	<div id="map-canvas"/>
     </div>
     
     <div id="sidr">
-	  <!-- Your content -->
+	  <!-- Menu items -->
 	  <ul>
-		<li><a href="#">Bookmarks</a></li>
-		<li class="active"><a href="#">Settings</a></li>
+		<li><a href="#"><i class="icon-bookmark icon-white"></i> &nbsp;&nbsp;Bookmarks</a></li>
+		<li><a href="#"><i class="icon-wrench icon-white"></i> &nbsp;&nbsp;Settings</a></li>
 	  </ul>
 	</div>
 	
@@ -54,10 +87,10 @@
 	<script src="js/jquery.sidr.min.js"></script>
 	
 	<script>
-$(document).ready(function() {
-  $('#simple-menu').sidr();
-});
-</script>
+		$(document).ready(function() {
+		  $('#simple-menu').sidr();
+		});
+	</script>
  
   </body>
 </html>
